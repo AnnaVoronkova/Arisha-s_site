48c6369cc8b3257adf8b75202860340007754318
AnnaVoronkova <63228705+AnnaVoronkova@users.noreply.github.com>
2025-08-02 21:38:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+let menuBtn = document.querySelector(".menu-btn");
+let nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+})
